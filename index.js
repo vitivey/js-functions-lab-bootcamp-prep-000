99f93b68439717ea4e2184b0636140427bb9008b
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-06 16:44:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 function happyHolidays() {return 'Happy holidays!'}
 function happyHolidaysTo(name) {return `Happy holidays ${name}!`}
 function happyHolidaysTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
-function holidayCountdown(holiday, days) {return `It/'s ${days} days until ${holiday}!`}
+function holidayCountdown(holiday, days) {return `It's ${days} days until ${holiday}!`}
